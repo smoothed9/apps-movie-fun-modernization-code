b890007650409f38d12e48b6576a49bfffe48bab
Sudeep Moothedath <smoothed9@gmail.com>
2017-12-19 18:35:14 -0500

    Database Migration

---
+++ databases/album-database/src/main/resources/db/migration/V1__create_album.sql
@@ -0,0 +1,8 @@
+create table album
+(
+  id     BIGINT(20) PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  artist VARCHAR(255),
+  rating INT(11)                NOT NULL,
+  title  VARCHAR(255),
+  year   INT(11)                NOT NULL
+);
